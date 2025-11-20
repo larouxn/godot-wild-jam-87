25cf1d203972f2e0f681974d2656bf1c65ad21fd
Nicholas La Roux <larouxn@gmail.com>
2025-11-20 17:17:13 -0500

    Add ignite and extinguish animations plus scripts to candles

--- Props/candle.gd
+++ Props/candle.gd
@@ -7,3 +7,11 @@ func _ready() -> void:
 	var animation_length: float = animation_player.get_animation("flicker").length
 	animation_player.play("flicker")
 	animation_player.seek(randf_range(0.0, animation_length))
+
+
+func ignite() -> void:
+	animation_player.play("ignite")
+
+
+func extinguish() -> void:
+	animation_player.play("extinguish")
--- Props/candle_tall.gd
+++
@@ -1,9 +0,0 @@
-extends StaticBody3D
-
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
-
-
-func _ready() -> void:
-	var animation_length: float = animation_player.get_animation("flicker").length
-	animation_player.play("flicker")
-	animation_player.seek(randf_range(0.0, animation_length))
