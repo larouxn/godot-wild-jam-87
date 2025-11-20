15037d990f53b84059ca2b9aa64c90c6f39ccf13
rhitakorrr <rhit@korrr>
2025-11-20 14:19:24 -0500

    Improve ControlView3D behavior
    - Add manual "Refresh Viewport" button to editor
    
    - Allow multiple Control nodes as children
    
    - Properly refresh in-editor when adding/removing child nodes
    
    - Show warning in-editor when there are no child Control nodes

--- UI/ControlView3D/control_view_3d.gd
+++ UI/ControlView3D/control_view_3d.gd
@@ -2,7 +2,10 @@
 
 class_name ControlView3D extends Node3D
 
+@export_tool_button("Refresh Viewport", "Reload") var refresh_fn := sync_viewport
+
 @onready var viewport := $Sprite3D/SubViewport as SubViewport
+
 
 func _ready() -> void:
 	# Set the viewport resolution to whatever the game viewport is set to.
@@ -11,35 +14,67 @@ func _ready() -> void:
 		ProjectSettings.get_setting("display/window/size/viewport_height"),
 	)
 
-	# If any of our children are Control nodes, move them into the viewport. Since
-	# we've setup the viewport to only ever have one child control, the last one
-	# takes precedence.
-	for child in get_children():
-		if child is Control:
-			move_child_control_to_viewport(child)
+	# Sync the viewport with our child Control nodes.
+	sync_viewport()
 
-	# Listen for new child nodes entering the tree.
+	# Listen for children entering/leaving the tree.
 	child_entered_tree.connect(_on_child_added)
+	child_exiting_tree.connect(_on_child_exiting)
 
 
-# If a child node is added and it is a Control node, move it to the viewport.
+# If a child node is added and it is a Control node, sync the viewport.
 func _on_child_added(node: Node) -> void:
 	if node is Control:
-		move_child_control_to_viewport(node)
+		sync_viewport()
+
+
+# If a child node is removed and it is a Control node, sync the viewport.
+func _on_child_exiting(node: Node) -> void:
+	if node is Control:
+		# Call deferred so the exiting node will have exited the tree before syncing.
+		sync_viewport.call_deferred()
+
 
 # Move a child node from the ControlView3D into the viewport. Behaves differently
 # depending on whether it's running in the editor or not.
-func move_child_control_to_viewport(node: Control) -> void:
-	# Remove other children of viewport first.
-	for child in viewport.get_children():
-		viewport.remove_child(child)
-		child.queue_free()
-
+func move_child_to_viewport(node: Control) -> void:
 	if Engine.is_editor_hint():
-		# Duplicate the node if we are running as an editor tool since trying to
-		# move it crashes the editor...
+		# Duplicate the node if we are running as an editor tool since we want the
+		# child node to remain available in the editor to modify.
 		viewport.add_child(node.duplicate())
 	else:
 		# Otherwise, just move the node to the viewport.
 		remove_child(node)
 		viewport.add_child(node)
+
+
+# Remove all children from the viewport.
+func clear_viewport() -> void:
+	for child in viewport.get_children():
+		viewport.remove_child(child)
+		child.queue_free()
+
+
+# Sync the viewport by clearing its children and moving our children that are
+# Control nodes into the viewport.
+func sync_viewport() -> void:
+	clear_viewport()
+
+	for child in get_children():
+		if child is Control:
+			move_child_to_viewport(child)
+
+
+# Show in-editor warnings for misconfigurations.
+func _get_configuration_warnings() -> PackedStringArray:
+	var warnings := []
+
+	var count := 0
+	for child in get_children():
+		if child is Control:
+			count += 1
+
+	if count < 1:
+		warnings.append("Missing child Control. Nothing will be rendered.")
+
+	return warnings
