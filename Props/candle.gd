larouxn/godot-wild-jam-87

commit 0390c2b7e06edf2a2fb6c0e8ba747daa0960b98b

Improve candle usage by moving flicker into ignite method after ignition

Props/candle.gd
@@ -4,13 +4,15 @@ extends StaticBody3D
 
 
 func _ready() -> void:
-	var animation_length: float = animation_player.get_animation("flicker").length
-	animation_player.play("flicker")
-	animation_player.seek(randf_range(0.0, animation_length))
+	ignite()
 
 
 func ignite() -> void:
 	animation_player.play("ignite")
+
+	var animation_length: float = animation_player.get_animation("flicker").length
+	animation_player.play("flicker")
+	animation_player.seek(randf_range(0.0, animation_length))
 
 
 func extinguish() -> void:

commit 4135aa847331a3f22bdcc4c837b60a21fb8e9a7d

Fix likely bug introduced in 0390c2b in which candle flicker instantly
In short, calling ignite animation then flicker animation does not guarantee than ignite finished before flicker starts as the latter does not wait. Thus, we can simply call flicker via a signal when we detect that ignite animation has finished.

Props/candle.gd
@@ -10,10 +10,17 @@ func _ready() -> void:
 func ignite() -> void:
 	animation_player.play("ignite")
 
+
+func extinguish() -> void:
+	animation_player.play("extinguish")
+
+
+func flicker() -> void:
 	var animation_length: float = animation_player.get_animation("flicker").length
 	animation_player.play("flicker")
 	animation_player.seek(randf_range(0.0, animation_length))
 
 
-func extinguish() -> void:
-	animation_player.play("extinguish")
+func _on_animation_player_animation_finished(animation_name: String) -> void:
+	if animation_name == "ignite":
+		flicker()

Props/candle_tall.tscn
@@ -110,3 +110,5 @@ libraries = {
 &"": SubResource("AnimationLibrary_uaool")
 }
 speed_scale = 0.5
+
+[connection signal="animation_finished" from="AnimationPlayer" to="." method="_on_animation_player_animation_finished"]