9f7b6f1af8be836e07651f23b64a9b83e54c1a87
Nicholas La Roux <larouxn@gmail.com>
2025-11-22 22:28:16 -0500

    Update hands with latest models, un-decay, and flip sides

--- Props/hands.gd
+++ Props/hands.gd
@@ -10,10 +10,11 @@ extends Node3D
 
 func _ready() -> void:
 	type()
+
 	hand_mesh_l.set_surface_override_material(0, hand_mesh_l.get_active_material(0).duplicate())
 	hand_mesh_r.set_surface_override_material(0, hand_mesh_r.get_active_material(0).duplicate())
 
-	update_decay_state(0.2)
+	set_decay(1.0)
 
 
 func type() -> void:
@@ -24,11 +25,7 @@ func cast() -> void:
 	animation_player.play("cast")
 
 
-func set_decay() -> void:
-	pass
-
-
-func update_decay_state(health_percentage: float) -> void:
+func set_decay(health_percentage: float) -> void:
 	var decay_amount: float = 1.0 - health_percentage
 	var new_color: Color = healthy_color.lerp(decay_color, decay_amount)
 
