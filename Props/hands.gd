779553426895a76c4f67045ba4d12798f4a3295e
Nicholas La Roux <larouxn@gmail.com>
2025-11-23 11:37:08 -0500

    Sync hands typing animation with actual player typing

--- Props/hands.gd
+++ Props/hands.gd
@@ -4,14 +4,20 @@ const COLOR_HEALTHY = Color.WHITE
 const COLOR_SICKLY = Color("#afb515")
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var animation_timer: Timer = $AnimationTimer
 @onready var hand_mesh_l: Node3D = $hand_left/Cube
 @onready var hand_mesh_r: Node3D = $hand_right/Cube
 @onready var player_health_bar: ProgressBar = $"../PlayerUI/ProgressBar"
+@onready var main_text_input_manager: InputManager = %InputManager
 
 
 func _ready() -> void:
 	hand_mesh_l.set_surface_override_material(0, hand_mesh_l.get_active_material(0).duplicate())
 	hand_mesh_r.set_surface_override_material(0, hand_mesh_r.get_active_material(0).duplicate())
+
+	main_text_input_manager.key_pressed.connect(_type)
+	animation_timer.timeout.connect(_idle)
+
 	_update_decay()
 
 
@@ -19,16 +25,18 @@ func _process(_delta: float) -> void:
 	_update_decay()
 
 
-func type() -> void:
+func _type() -> void:
 	animation_player.play("type")
+	animation_timer.start()
 
 
-func cast() -> void:
+func _cast() -> void:
+	animation_timer.stop()
 	animation_player.play("cast")
 
 
-func idle() -> void:
-	# TODO: try keep_state: true for resumable animations
+func _idle() -> void:
+	animation_timer.stop()
 	animation_player.stop()
 
 
