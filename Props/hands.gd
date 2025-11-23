90dc3b97ed6abb18754da13480c226be235d11b2
Nicholas La Roux <larouxn@gmail.com>
2025-11-23 10:52:51 -0500

    Update hands to sync color i.e. decay alongside player health

--- Props/hands.gd
+++ Props/hands.gd
@@ -1,20 +1,22 @@
 extends Node3D
 
-@export var healthy_color: Color = Color.WHITE
-@export var decay_color: Color = Color(0.2, 0.8, 0.2)
+const COLOR_HEALTHY = Color.WHITE
+const COLOR_SICKLY = Color("#afb515")
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var hand_mesh_l: Node3D = $hand_left/Cube
 @onready var hand_mesh_r: Node3D = $hand_right/Cube
+@onready var player_health_bar: ProgressBar = $"../PlayerUI/ProgressBar"
 
 
 func _ready() -> void:
-	type()
-
 	hand_mesh_l.set_surface_override_material(0, hand_mesh_l.get_active_material(0).duplicate())
 	hand_mesh_r.set_surface_override_material(0, hand_mesh_r.get_active_material(0).duplicate())
+	_update_decay()
 
-	set_decay(1.0)
+
+func _process(_delta: float) -> void:
+	_update_decay()
 
 
 func type() -> void:
@@ -25,9 +27,15 @@ func cast() -> void:
 	animation_player.play("cast")
 
 
-func set_decay(health_percentage: float) -> void:
-	var decay_amount: float = 1.0 - health_percentage
-	var new_color: Color = healthy_color.lerp(decay_color, decay_amount)
+func idle() -> void:
+	# TODO: try keep_state: true for resumable animations
+	animation_player.stop()
+
+
+func _update_decay() -> void:
+	var ratio: float = player_health_bar.value / player_health_bar.max_value
+	var weighted_ratio: float = ease(ratio, 0.4)  # Transition colors slower
+	var new_color: Color = COLOR_SICKLY.lerp(COLOR_HEALTHY, weighted_ratio)
 
 	hand_mesh_l.get_surface_override_material(0).albedo_color = new_color
 	hand_mesh_r.get_surface_override_material(0).albedo_color = new_color
