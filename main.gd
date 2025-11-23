9c21112856e7cd7114e36f86ec07a37d4017070b
wboccard <william.boccard@gmail.com>
2025-11-22 23:39:35 -0500

    added animation

--- main.gd
+++ main.gd
@@ -5,23 +5,26 @@ extends Node3D
 var cursor_text: CursorText
 var main_text := TextState.new()
 var looking_at_book := false
+var health: float
 
 @onready var main_text_container := $"MainText" as ControlView3D
-#@onready var animation_player := $AnimationPlayer as AnimationPlayer
+@onready var animation_player := $AnimationPlayer as AnimationPlayer
+@onready var player_ui := $PlayerUI as Control
+@onready var health_bar := $PlayerUI/ProgressBar as ProgressBar
 
 
 func _ready() -> void:
 	init_nodes.call_deferred()
 	input_manager.set_main_text(main_text)
-	#input_manager.input_state_changed.connect(
-	#func(input_state: InputManager.InputState) -> void:
-	#if input_state == InputManager.InputState.MAIN_TEXT:
-	#looking_at_book = false
-	#animation_player.play("look_typewriter")
-	#elif !looking_at_book:
-	#looking_at_book = true
-	#animation_player.play("look_book")
-	#)
+	input_manager.input_state_changed.connect(
+		func(input_state: InputManager.InputState) -> void:
+			if input_state == InputManager.InputState.MAIN_TEXT:
+				looking_at_book = false
+				animation_player.play("look_typewriter")
+			elif !looking_at_book:
+				looking_at_book = true
+				animation_player.play("look_book")
+	)
 	input_manager.selection_mistyped.connect(
 		func(_candidates: Array[TextState]) -> void: input_manager.set_active_text(main_text)
 	)
@@ -41,3 +44,8 @@ func init_nodes() -> void:
 			await get_tree().create_timer(3.0).timeout
 			main_text.unlock()
 	)
+
+
+func _process(_delta: float) -> void:
+	if health <= 0:
+		print("Game Over!")
